3583cffefa965b9ee4038248dbc4642b5d1b8cb0
MakrFrost <frostmark@ukr.net>
2022-06-04 18:03:33 +0300

    Adaptive 768px

--- js/modal.js
+++ js/modal.js
@@ -14,20 +14,3 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
-
-// /Мобильное окно с контактными данными/
-
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
-  };
-
-  refs.openMenuBtn.addEventListener('click', toggleModal);
-  refs.closeMenuBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.menu.classList.toggle('is-open');
-  }
-})();
